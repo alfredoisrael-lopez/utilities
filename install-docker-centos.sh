9090d52e7197918334f778059e466607e01da507
alfredoisrael-lopez <alfredoisrael-lopez@users.noreply.github.com>
2019-01-04 10:45:27 -0600

    erase special characters and new lines

--- install-docker-centos.sh
+++ install-docker-centos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum-config-manager --enable docker-ce-edge
