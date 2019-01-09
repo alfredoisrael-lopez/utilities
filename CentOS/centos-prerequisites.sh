679c289d549ae9960f2eb07c366f73eda2e21315
alfredo-lopez <alfredo.lopez@mx1.ibm.com>
2019-01-09 12:44:22 -0600

    re organization of the repository

---
+++ CentOS/centos-prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+yum -y update
+yum -y install nfs-utils python socat
--- CentOS/install-docker-centos.sh
+++ CentOS/install-docker-centos.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-yum -y update
+
 yum -y remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
